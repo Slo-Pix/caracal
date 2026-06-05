4790ee7f6894d8409ec9d11de517788c81ac7e94
RAWx18 <rawx18.dev@gmail.com>
2026-06-05 15:10:21 +0530

    Remove deprecated mock files for FX rates and compliance stream

--- examples/lynxCapital/_mock/grpc/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
-Caracal, a product of Garudex Labs
-
-gRPC mock servers package.
-"""
--- examples/lynxCapital/_mock/grpc/compliance_stream/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
-Caracal, a product of Garudex Labs
-
-compliance_stream gRPC server package.
-"""
